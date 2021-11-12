e3eeb7da8eee5e032020efafe87efc493638d8e2
iravieira <89557346+iravieira@users.noreply.github.com>
2021-11-12 18:28:04 -0300

    Add files via upload
    Checkpoint

---
+++ projetomicroondas.js
@@ -0,0 +1,42 @@
+// menu -> escolha e opções
+
+// QUAIS OPÇÕES?
+// PRATOS
+// 1 A 5
+
+// 1 - PIPOCA
+
+// 2 - MACARRÃO
+
+// 3 - CARNE
+
+// 4 - FEIJÃO
+
+// 5 - BRIGADEIRO
+
+
+
+function qualOprato(valor){
+      //1 a 5
+      switch (valor){
+            case '1': 
+            console.log("PIPOCA");
+            break;
+            case '2':
+            console.log("MACARRÃO");
+            break;
+            case '3':
+            console.log("CARNE");
+            break;
+        case '4':
+            console.log("FEIJÃO");
+            break;
+        case '5':
+            console.log("BRIGADEIRO");
+            break;
+
+      }
+}
+
+qualOprato('1');
+qualOprato('3');
